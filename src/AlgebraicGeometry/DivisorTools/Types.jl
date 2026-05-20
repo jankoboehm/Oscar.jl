4fe8336880eaa82c4727e94679d404ee9c403871
Janko Boehm <boehm@mathematik.uni-kl.de>
2026-05-20 15:35:21 +0200

    divisor stuff

---
+++ src/AlgebraicGeometry/DivisorTools/Types.jl
@@ -0,0 +1,35 @@
+struct EmbeddedDivisorAmbient{T}
+  R::Any
+  X::MPolyIdeal{T}
+  projective::Bool
+  irrelevant::Union{Nothing, MPolyIdeal{T}}
+end
+
+struct EmbeddedDivisor{T}
+  ambient::EmbeddedDivisorAmbient{T}
+  num::MPolyIdeal{T}
+  den::MPolyIdeal{T}
+end
+
+struct EmbeddedDivisorSections{T}
+  divisor::EmbeddedDivisor{T}
+  numerator::MPolyIdeal{T}
+  denominator::T
+end
+
+struct EmbeddedGradedModuleSections{T}
+  ambient::EmbeddedDivisorAmbient{T}
+  module_object::Any
+  numerator::MPolyIdeal{T}
+  denominator::T
+  sheaf_degree::Any
+  numerator_degree::Any
+  vector_space::Any
+  embedding::Any
+  numerators::Vector{T}
+end
+
+struct EmbeddedFormalDivisor{T}
+  ambient::EmbeddedDivisorAmbient{T}
+  summands::Vector{Tuple{Int, EmbeddedDivisor{T}}}
+end
